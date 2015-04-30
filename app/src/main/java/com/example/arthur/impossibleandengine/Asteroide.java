49a26c208f0338f7ec98a7c1ba32319139111e5f
Marcus Vinicius <marcusvinicius30798@gmail.com>
2015-04-30 17:45:48 -0300

    removida arquitetura antiga

--- app/src/main/java/com/example/arthur/impossibleandengine/Asteroide.java
+++
@@ -1,4 +0,0 @@
-package com.example.arthur.impossibleandengine;
-
-public class Asteroide {
-}
--- app/src/main/java/com/example/arthur/impossibleandengine/GameOver.java
+++
@@ -1,7 +0,0 @@
-package com.example.arthur.impossibleandengine;
-
-/**
- * Created by arthur on 24/03/2015.
- */
-public class GameOver {
-}
